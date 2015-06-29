dc2bf3829d125f839f36fa98c53348c8e5007657
Jay Quigley <quigley.james.g@gmail.com>
2015-06-29 03:19:15 -0400

    new text for c9; update replacetitle.js to reflect purple color; adjust title & post_title ids in t1; very minor spacing in blog-stylesheet-keyboard-t7.

--- js/replacetitle.js
+++ js/replacetitle.js
@@ -1,1 +1,2 @@
-document.getElementById("post_title").innerHTML = (document.getElementById("title").innerHTML)
+document.getElementById("post_title").innerHTML = (document.getElementById("title").innerHTML);
+document.getElementById("post_title").style.color = "#36417d";
