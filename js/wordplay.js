016baf92bb8ec8d922ddcb5021986f1bf7cf62c7
Jay Quigley <quigley.james.g@gmail.com>
2015-10-01 00:49:26 -0500

    refactor all the wordplay JS code to modular jQuery

--- js/wordplay.js
+++ js/wordplay.js
@@ -3,18 +3,13 @@ $(function() {
   var currentAdj = randomAdj();
   $("#making-verb").html(currentVerb);
   $("#persev-adj").html(currentAdj);
+
   newDistinctVerbOnHover(currentVerb);
   newDistinctAdjOnHover(currentAdj);
-});
 
-document.getElementById('place-made').onmouseover = function() {
-  sayWhere();
-  document.getElementById('place-made').style.color = "#36417d";
-};
-document.getElementById('place-made').onmouseout = function() {
-  heartOfMidwest();
-  document.getElementById('place-made').style.color = "#333333";
-};
+  identifyHeartOfMidwestOnMouseEnter();
+  backtoHeartOfMidwestOnMouseOut();
+});
 
 function findDistinctVerb(currentVerb) {
   var newVerb = randomVerb();
@@ -25,11 +20,8 @@ function findDistinctVerb(currentVerb) {
 }
 function findDistinctAdj(currentAdj) {
   var newAdj = randomAdj();
-  console.log("outside", currentAdj, newAdj);
   while (currentAdj == newAdj) {
-    console.log("inside-1", currentAdj, newAdj);
     newAdj = randomAdj();
-    console.log("inside-2", currentAdj, newAdj);
   }
   return newAdj;
 }
@@ -45,6 +37,17 @@ function newDistinctAdjOnHover(currentAdj) {
     var newAdj = findDistinctAdj(currentAdj);
     $(this).html(newAdj);
     currentAdj = newAdj;
+  });
+}
+
+function identifyHeartOfMidwestOnMouseEnter() {
+  $("#place-made").on("mouseenter", function(){
+    $(this).html("Chicago, Illinois and Dayton, Ohio").css("color", "#36417d");
+  });
+}
+function backtoHeartOfMidwestOnMouseOut() {
+  $("#place-made").on("mouseout", function(){
+    $(this).html("the heart of the Midwest").css("color", "initial");
   });
 }
 
@@ -103,11 +106,5 @@ var randomVerb = function() {
 var randomAdj = function() {
   return adjectives[Math.floor(Math.random()*adjectives.length)];
 };
-var sayWhere = function() {
-  document.getElementById('place-made').innerHTML = "Chicago, Illinois and Dayton, Ohio";
-}
-var heartOfMidwest = function() {
-  document.getElementById('place-made').innerHTML = "the heart of the Midwest";
-}
 
 
