e31f52ad0d0d59d73f4c1a83a99ee320597c36df
Jay Quigley <quigley.james.g@gmail.com>
2015-11-13 16:50:02 -0600

    refactor JS file to handle project description panes

--- js/description-hover.js
+++ js/description-hover.js
@@ -1,43 +1,14 @@
-$(function() {
-	$("#pollarity-description").on("click", function(event) {
-		event.preventDefault();
-		$("#pollarity-blurb").dialog({
-			modal: true,
-			resizable: true,
-			minWidth: 600,
-		});
-	});
-	$("#trollhunter-description").on("click", function(event) {
-		event.preventDefault();
-		$("#trollhunter-blurb").dialog({
-			modal: true,
-			resizable: true,
-			minWidth: 600,
-		});
-	});
-	$("#rotpot-description").on("click", function(event) {
-		event.preventDefault();
-		$("#rotpot-blurb").dialog({
-			modal: true,
-			resizable: true,
-			minWidth: 600,
-		});
-	});
-	$("#this-site-description").on("click", function(event) {
-		event.preventDefault();
-		$("#this-site-blurb").dialog({
-			modal: true,
-			resizable: true,
-			minWidth: 600,
-		});
-	});
-	$("#qool-quotes-description").on("click", function(event) {
-		event.preventDefault();
-		$("#qool-quotes-blurb").dialog({
-			modal: true,
-			resizable: true,
-			minWidth: 600,
-		});
-	});
+var tagNames =  ["#pollarity", "#trollhunter", "#rotpot", "#this-site", "#qool-quotes"];
 
+$(function() {
+	tagNames.forEach(function(element) {
+		$(element + "-description").on("click", function(event) {
+			event.preventDefault();
+			$(element + "-blurb").dialog({
+				modal: true,
+				resizable: true,
+				minWidth: 600,
+			});
+		});		
+	});
 });
